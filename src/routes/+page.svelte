7a7e0256ceb5b294a96e79ea63a143b7da5dfaf4
Konstantin <humbl3dev@gmail.com>
2025-10-20 16:51:35 -0700

    Refactor login and signup links to use button component variants
    - Updated the login and signup links in the landing page to utilize the new button component with customizable variants for improved styling and consistency.
    - Replaced static class names with dynamic class generation using buttonVariants for better maintainability.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,22 @@
+<script>
+	import { buttonVariants } from '$lib/components/ui/button';
+</script>
+
 <div class="flex h-dvh items-center justify-center">
 	<div>
 		<h1 class="mb-4 text-4xl font-bold">Freelance Management Portal</h1>
 		<div class="grid grid-cols-2 gap-2">
 			<a
 				href="/auth/login"
-				class="w-full rounded-sm border-transparent bg-blue-500 p-2 text-center text-white">Login</a
+				class={`cursor-pointer ${buttonVariants({
+					variant: 'default'
+				})}`}>Login</a
 			>
 			<a
 				href="/auth/signup"
-				class="w-full rounded-sm border border-black bg-transparent p-2 text-center text-black"
-				>Sign Up</a
+				class={`cursor-pointer ${buttonVariants({
+					variant: 'outline'
+				})}`}>Sign Up</a
 			>
 		</div>
 	</div>
