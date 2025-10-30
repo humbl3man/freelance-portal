e244d5ba1a71fd966be097df6f8a24079cc19bf9
Konstantin <humbl3dev@gmail.com>
2025-10-30 13:10:37 -0700

    Refactor login options rendering in +page.svelte
    - Introduced conditional rendering for login options based on email/password authentication availability.
    - Enhanced layout and styling of the login and sign-up buttons for improved user experience.
    - Updated the main heading for better visual hierarchy.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,51 @@
 <script>
 	import { buttonVariants } from '$lib/components/ui/button';
+	import { cn } from '$lib/utils.js';
 	const { data } = $props();
 	const { isEmailPasswordEnabled } = data;
 </script>
 
-<div class="flex h-dvh items-center justify-center">
+{#snippet loginOptionsWithoutEmailPassword()}
+	<div class={cn('grid items-center justify-center')}>
+		<a
+			href="/auth/login"
+			class={`cursor-pointer ${buttonVariants({
+				variant: 'default',
+				size: 'lg'
+			})}`}
+		>
+			Get Started
+		</a>
+		{#if isEmailPasswordEnabled}{/if}
+	</div>
+{/snippet}
+
+{#snippet loginOptionsWithEmailPassword()}
+	<div class={cn('grid grid-cols-2 gap-2')}>
+		<a
+			href="/auth/signup"
+			class={`cursor-pointer ${buttonVariants({
+				variant: 'outline'
+			})}`}>Sign Up</a
+		>
+		<a
+			href="/auth/login"
+			class={`cursor-pointer ${buttonVariants({
+				variant: 'default'
+			})}`}
+		>
+			Login
+		</a>
+	</div>
+{/snippet}
+
+<div class="flex h-dvh items-center justify-center bg-blue-50">
 	<div>
-		<h1 class="mb-4 text-4xl font-bold">Freelance Management Portal</h1>
-		<div class="grid grid-cols-2 gap-2">
-			<a
-				href="/auth/login"
-				class={`cursor-pointer ${buttonVariants({
-					variant: 'default'
-				})}`}>Login</a
-			>
-			{#if isEmailPasswordEnabled}
-				<a
-					href="/auth/signup"
-					class={`cursor-pointer ${buttonVariants({
-						variant: 'outline'
-					})}`}>Sign Up</a
-				>
-			{/if}
-		</div>
+		<h1 class="mb-8 text-5xl font-semibold">Freelance Management Portal</h1>
+		{#if isEmailPasswordEnabled}
+			{@render loginOptionsWithEmailPassword()}
+		{:else}
+			{@render loginOptionsWithoutEmailPassword()}
+		{/if}
 	</div>
 </div>
